a1abccbbbaa6db30573d4b1d44b1d9e17b4c36ce
César Rafael <cesarrdev@gmail.com>
2023-02-11 12:11:16 -0300

    feat: created components styled of MenuNav

--- portfolio/src/components/MenuNav/index.tsx
+++ portfolio/src/components/MenuNav/index.tsx
@@ -1,6 +1,30 @@
-import { IChildren } from "../../interfaces/props";
 import { SectionMenuNavStyled } from "./style";
+import LogoAssasins from "../../assets/img/icons/assasin_icon.png";
+import CloseArrow from "../../assets/img/icons/closeArror-2.png";
 
-export const MenuNav = ({ children }: IChildren) => {
-  return <SectionMenuNavStyled>{children}</SectionMenuNavStyled>;
+export const MenuNav = () => {
+  return (
+    <SectionMenuNavStyled>
+      <nav>
+        <figure>
+          <img src={LogoAssasins} alt="Assasins Icon" />
+          <img
+            src={CloseArrow}
+            alt="Assasins Icon"
+            className="btn__closeArrow"
+          />
+        </figure>
+        <a href="#">Informações</a>
+        <span className="lineNav"></span>
+        <a>Habilidades</a>
+        <span className="lineNav"></span>
+        <a>Experiência</a>
+        <span className="lineNav"></span>
+        <a>Projetos</a>
+        <span className="lineNav"></span>
+        <a>Contato</a>
+        <span className="lineNav"></span>
+      </nav>
+    </SectionMenuNavStyled>
+  );
 };
